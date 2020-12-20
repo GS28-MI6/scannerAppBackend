f3072c472dac2eef196bf31a07341b54680520be
GS28-MI6 <gasosa2811@gmail.com>
2020-12-20 00:12:47 -0300

    fixed more users

--- routes/appData.js
+++ routes/appData.js
@@ -136,7 +136,7 @@ module.exports = server => {
 
       console.log(precio, stock, barcode)
 
-          connection.query("SELECT * FROM productos WHERE barcode=?",[barcode], function(error, results, fields){
+          connection.query("SELECT * FROM productos WHERE barcode=? AND cliente=?",[barcode, id_cliente], function(error, results, fields){
             console.log(results, "hi there")
             if (results[0] === undefined){
               connection.query(
@@ -190,78 +190,9 @@ module.exports = server => {
     }
   });
 
-  server.post("/count", function(req, res) {
-
-    var { desde, hasta } = req.body
-
-    if (desde === undefined || desde === ""){
-      desde = "2019-12-10"
-    }
-    if(hasta === undefined || hasta === ""){
-      hasta = new Date().toISOString().split('T')[0]
-    }
-
-    console.log(desde, hasta)
-
-    connection.query("SELECT COUNT(*) AS total_alertas FROM alertas WHERE ingreso >= ? AND ingreso <= ?",
-    [desde,hasta],
-     function(
-      error,
-      results,
-      fields
-    ) {
-      var total = results;
-      if (error) throw error;
-      console.log(error, results);
-      connection.query(
-        "SELECT tipo,COUNT(*) AS all_alertas FROM alertas WHERE ingreso >= ? AND ingreso <= ? GROUP BY tipo ORDER BY tipo ASC",
-        [desde,hasta],
-        function(err, ress, field) {
-          console.log(ress);
-          var alertsCount = {
-            total: total[0].total_alertas,
-            accidente_vial: 0,
-            bomberos: 0,
-            disturbios: 0,
-            robo: 0,
-            salud: 0,
-            violencia_genero: 0
-          };
-          ress.map(function(alert) {
-            console.log(alert);
-            switch (alert.tipo) {
-              case "Accidente Vial":
-                alertsCount.accidente_vial = alert.all_alertas;
-                break;
-              case "Bomberos":
-                alertsCount.bomberos = alert.all_alertas;
-                break;
-              case "Disturbios":
-                alertsCount.disturbios = alert.all_alertas;
-                console.log(alertsCount);
-                break;
-              case "Robo":
-                alertsCount.robo = alert.all_alertas;
-                break;
-              case "Salud":
-                alertsCount.salud = alert.all_alertas;
-                console.log(alertsCount);
-                break;
-              case "Violencia de Género":
-                alertsCount.violencia_genero = alert.all_alertas;
-                break;
-              default:
-                console.log("default");
-                break;
-            }
-          });
-          res.send(200, alertsCount);
-        }
-      );
-    });
-  });
-
   server.post("/countFiltered", function(req, res) {
+
+    const {id_cliente} = req.body.data
 
     // var {tipo, desde, hasta} = req.body
 
@@ -273,7 +204,8 @@ module.exports = server => {
     // }
 
     connection.query(
-      "SELECT SUM(total_venta) AS total_venta, DATE(fecha_venta) AS fecha_venta FROM scanner_app_db.ventas group by DATE(fecha_venta) ORDER BY DATE(fecha_venta) ASC",
+      "SELECT SUM(total_venta) AS total_venta, DATE(fecha_venta) AS fecha_venta FROM scanner_app_db.ventas WHERE cliente=? group by DATE(fecha_venta) ORDER BY DATE(fecha_venta) ASC",
+      [id_cliente],
       function(error, results, fields) {
         if (error) throw error;
         console.log(results)
